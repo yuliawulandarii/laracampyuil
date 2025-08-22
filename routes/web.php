65b09db9b86df731c563a7773c488d388c804935
yuliawulandarii <yuliawulandari1907@gmail.com>
2025-08-22 16:34:29 +0700

    integration landing page, checkout, and my dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\User\CheckoutController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,20 +20,22 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('checkout', function () {
-    return view('checkout');
-})->name('checkout');
+Route::middleware(['auth'])->group(function (){
+    //checkout routes
+    Route::get('checkout/success', [CheckoutController::class,'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class,'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class,'store'])->name('checkout.store');
 
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
+    //user dashboard
+    Route::get('dashboard', [HomeController::class,'dashboard'])->name('dashboard');
+});
 
 //socialite routes
 Route::get('sign-in-google', [UserController::class,'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class,'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
